9905044f205861a8ee0f8f21dbd4a1ceed8dd274
ramzi <ramzikvrafikv@gmail.com>
2021-05-23 13:48:53 +0530

    increment butten to increase count

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Counter() {
+    const [count,setCount]=useState(0)
     return (
         <div>
-            <h1> Hello am a component</h1>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <h1> Hello am a component : {count}</h1>
         </div>
     )
 }
